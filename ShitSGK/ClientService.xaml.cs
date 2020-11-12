346b149acc606ec5aabb831c6444ec51f85994fa
РИШАТ Ахтямов <rishat-rus@mail.ru>
2020-11-12 11:17:23 +0400

    1

---
+++ ShitSGK/ClientService.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ShitSGK
+{
+    /// <summary>
+    /// Логика взаимодействия для ClientService.xaml
+    /// </summary>
+    public partial class ClientService : Window
+    {
+        public ClientService()
+        {
+            InitializeComponent();
+            Database.masterEntities entities = new Database.masterEntities();
+            dataGridMain.ItemsSource = entities.ClientService.ToList();
+        }
+    }
+}
